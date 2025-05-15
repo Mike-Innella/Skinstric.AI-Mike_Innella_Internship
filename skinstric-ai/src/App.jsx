467408b129e02bf52b09b7598fb7239440696573
Michael Innella <mainnella@gmail.com>
2025-05-15 13:14:50 -0400

    Update App.jsx
    Added note to self for project continuation later.

--- skinstric-ai/src/App.jsx
+++ skinstric-ai/src/App.jsx
@@ -69,3 +69,5 @@ function App() {
 }
 
 export default App;
+
+// TODO: Notes for later: Create "Enter Code" modal, address submit issue with form inputs, address - when previous step input info is held (good) however the next arrow isn't present unless reenter input, finish phase 1.
