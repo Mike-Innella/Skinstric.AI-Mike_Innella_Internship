691ae2d406cbcebb8a361114f7f75ed5d64f8d9b
Michael Innella <62763951+Mike-Innella@users.noreply.github.com>
2025-05-21 16:19:31 -0400

    update page navigation on dashboard

--- skinstric-ai/src/Pages/DashboardPage.jsx
+++ skinstric-ai/src/Pages/DashboardPage.jsx
@@ -4,8 +4,19 @@ import Dashboard from "../Components/Dashboard";
 import "../UI/Styles/Pages/Pages.css";
 import "../UI/Styles/Pages/DashboardPage.css";
 import PageBoxes from "../Components/PageBoxes";
+import { useNavigate } from "react-router-dom";
 
 function DashboardPage() {
+  const navigate = useNavigate();
+
+  const handleNext = () => {
+    navigate("/analysis");
+  };
+
+  const handleBack = () => {
+    navigate("/");
+  };
+
   return (
     <>
       <Helmet>
@@ -18,6 +29,10 @@ function DashboardPage() {
         showCenter={true}
         showNextButton={true}
         showBackButton={true}
+        backButtonLabel="BACK"
+        nextButtonLabel="GET SUMMARY"
+        onNext={handleNext}
+        onBack={handleBack}
       />
 
       <Dashboard />
